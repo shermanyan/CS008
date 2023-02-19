39009ffbbce9fa7df21d22b7a04d3fe735196eed
Sherman Yan <syan1689@gmail.com>
2023-02-18 22:06:17 -0800

    update

--- review/cmake-build-debug/CMakeFiles/ucs8_spring23_review_stubs_and_drivers.dir/compiler_depend.ts
+++
@@ -1,2 +0,0 @@
-# CMAKE generated file: DO NOT EDIT!
-# Timestamp file for compiler generated dependencies management for ucs8_spring23_review_stubs_and_drivers.
--- review/cmake-build-debug/CMakeFiles/ucs8_spring23_review_stubs_and_drivers.dir/depend.make
+++
@@ -1,2 +0,0 @@
-# Empty dependencies file for ucs8_spring23_review_stubs_and_drivers.
-# This may be replaced when dependencies are built.
